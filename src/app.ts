52f5d0d6fa1e72fc8547fd1c04f54f62f2b34df2
Justin Schweigert <jschweigert@mimeo.com>
2018-08-03 10:07:08 -0500

    Resolve merge conflicts.

---
+++ src/app.ts
@@ -0,0 +1,59 @@
+import { VssPullRequests } from "./vss-pull-requests.service";
+declare var sorttable: any;
+
+const prClient = new VssPullRequests();
+prClient.getPullRequests().then(prs => {
+    // Build the table
+    const tableHeader = document.getElementById("pr-header");
+    tableHeader.innerText += " (" + prs.length + " pull requests)";
+    const tableBody = document.getElementById("pr-body");
+    console.log("*** pull request data ***", prs);
+    prs.forEach(pr => {
+        const tableRow = document.createElement("tr");
+        tableRow.classList.add("table-row");
+        // User Avatar cell
+        const tableCellUserAvatar = document.createElement("td");
+        tableCellUserAvatar.innerHTML = "<img class='user-avatar' src='" + pr.createdBy.imageUrl + "' alt='" + pr.createdBy.uniqueName + "\'s avatar' />";
+        tableRow.appendChild(tableCellUserAvatar);
+        // Created By cell
+        const tableCellUser = document.createElement("td");
+        tableCellUser.innerText = pr.createdBy.displayName;
+        tableRow.appendChild(tableCellUser);
+        // Pull Request ID cell
+        const tableCellId = document.createElement("td");
+        tableCellId.innerHTML = "<a href='" + pr.url + "' target='_top'>Pull Request #" + pr.id + "</a>";
+        tableRow.appendChild(tableCellId);
+        // Title cell
+        const tableCellTitle = document.createElement("td");
+        tableCellTitle.innerText = pr.title;
+        tableRow.appendChild(tableCellTitle);
+        // Repository cell
+        const tableCellRepo = document.createElement("td");
+        tableCellRepo.innerText = pr.repo;
+        tableRow.appendChild(tableCellRepo);
+        // My Vote cell
+        const tableCellVote = document.createElement("td");
+        const vote = prClient.voteNumberToVote(pr.vote);
+        tableCellVote.innerHTML = vote.icon + " " + vote.message;
+        if (vote.color !== undefined) {
+            tableCellVote.style.color = vote.color;
+        }
+        tableRow.appendChild(tableCellVote);
+        // Reviewers cell
+        const tableCellReviewers = document.createElement("td");
+        tableCellReviewers.classList.add("reviewers-cell");
+        pr.reviewers.forEach(reviewer => {
+            const reviewerVote = prClient.voteNumberToVote(reviewer.vote);
+            const reviewerElement = document.createElement("span");
+            reviewerElement.classList.add("reviewer-icon");
+            reviewerElement.innerHTML += "<img class='user-avatar' src='" + reviewer.imageUrl + "' alt=\"" + reviewer.uniqueName + "'s avatar\" />";
+            reviewerElement.innerHTML += "<div class='vote-icon'>" + reviewerVote.icon + "</div>";
+            tableCellReviewers.appendChild(reviewerElement);
+        });
+        tableRow.appendChild(tableCellReviewers);
+        tableBody.appendChild(tableRow);
+    });
+}).then(() => sorttable.makeSortable(document.getElementById("pr-table"))).then(() => {
+    const prIdHeader = document.getElementsByClassName("pr-id-header")[0];
+    sorttable.innerSortFunction.apply(prIdHeader, []);
+});
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+var path = require("path");
+var webpack = require("webpack");
+
+module.exports = {
+    target: "web",
+    entry: {
+        app: "./src/app.ts"
+    },
+    output: {
+        filename: "[name].js",
+        libraryTarget: "amd"
+    },
+    externals: [
+        /^VSS\/.*/, /^TFS\/.*/, /^q$/
+    ],
+    resolve: {
+        extensions: ["*",".webpack.js", ".web.js", ".ts", ".tsx", ".js"],
+        modules: [path.resolve("./src"),"node_modules"]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                enforce: "pre",
+                loader: "tslint-loader",
+                options: {
+                    emitErrors: true,
+                    failOnHint: true
+                }
+            },
+            {
+                test: /\.tsx?$/,
+                loader: "ts-loader"
+            }
+        ]
+    }
+}
