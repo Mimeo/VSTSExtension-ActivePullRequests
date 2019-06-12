bebb92839e84ce8ce20fad72893491d8c1d13222
Nithin Shenoy <nithin_shenoy@hotmail.com>
2019-06-12 14:20:41 -0400

    Moves message to it's own HTML element

--- src/app.ts
+++ src/app.ts
@@ -76,7 +76,9 @@ prClient.getPullRequests().then(prs => prClient.applyLatestBuilds(prs)).then(prB
 }).then(() => sorttable.makeSortable(document.getElementById("pr-table"))).then(() => {
     const prIdHeader = document.getElementsByClassName("pr-id-header")[0];
     sorttable.innerSortFunction.apply(prIdHeader, []);
+    // remove special message node
+    const specialMessageNode = document.getElementById("special-message");
+    specialMessageNode.parentNode.removeChild(specialMessageNode);
     document.getElementById("pr-body").classList.remove("loading");
-    document.getElementById("temphack").classList.remove("loading2");
     document.getElementById("pr-body").classList.add("loaded");
 });
